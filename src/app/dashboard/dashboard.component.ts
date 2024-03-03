66375f1a13c07591ddae13b792ff4e08bf90ec37
mattia33011 <mattia.iaria30@gmail.com>
2024-03-03 08:31:12 +0100

    i18n implemented

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CardComponent } from '../card/card.component';
-import { faAngular, faDocker, faEnvira, faGithub, faInstagram, faJava, faJs, faLinkedin, faLinkedinIn, faReact, faThreads } from '@fortawesome/free-brands-svg-icons';
+import { faAngular, faDocker, faGithub, faInstagram, faJava, faJs, faLinkedin, faLinkedinIn, faReact, faThreads } from '@fortawesome/free-brands-svg-icons';
 import { faCopyright } from '@fortawesome/free-regular-svg-icons';
 import { faDatabase, faLeaf } from '@fortawesome/free-solid-svg-icons';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, FontAwesomeModule, CardComponent],
+  imports: [HeaderComponent, FontAwesomeModule, CardComponent, TranslateModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
@@ -38,7 +40,6 @@ export class DashboardComponent {
     {title: '@mattiaiaria', icon: faInstagram, href: 'https://www.instagram.com/mattiaiaria/'},
     {title: '@mattiaiaria', icon: faThreads, href: 'https://www.threads.net/@mattiaiaria'},
     {title: 'mattia33011', icon: faGithub, href: 'https://github.com/mattia33011'},
-
   ]
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { LanguageDropDownComponent } from '../language-drop-down/language-drop-down.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LanguageDropDownComponent, TranslateModule],
+  imports: [LanguageDropDownComponent, TranslateModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   linkList = [
-    {id: "aboutMe", label: "About me"},
+    {id: "aboutMe", label: "AboutMe"},
     {id: "contacts", label: "Contacts"},
     {id: "experiences", label: "Experiences"},
     {id: "socials", label: "Socials"},
